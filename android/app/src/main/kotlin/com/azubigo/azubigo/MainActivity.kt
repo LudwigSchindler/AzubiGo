ab87dac712ecd42672c46eb22a5b1c62cd223950
Karl Jimmy Götzschel <jimmy.goetzschel@gmail.com>
2023-01-26 21:46:46 +0100

    :) accept my pull request

---
+++ android/app/src/main/kotlin/com/azubigo/azubigo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.azubigo.azubigo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/service/database_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class DatabaseService{
-
-  static final FirebaseFirestore instance = FirebaseFirestore.instance;
-}
